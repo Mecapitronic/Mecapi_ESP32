2bf3009bf734ce765f0081119094b562fc85014e
Geoffroy <117825952+Geoffroy-Milton-Innovation@users.noreply.github.com>
2023-03-14 14:11:04 +0100

    Implement multitask for both core

--- include/main.h
+++ include/main.h
@@ -6,10 +6,11 @@
 #define SERIAL_PC Serial
 #define SERIAL_LIDAR Serial2
 
-
 #include <LinkedList.h>
 #include "LD06.h"
 
 // #include "LD06Driver.h"
+void Task1code(void *pvParameters);
+void Task2code(void *pvParameters);
 
 #endif
