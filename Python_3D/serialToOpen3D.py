8a38bc108d563d55c615c82f8b0b5949cacc1263
Geoffroy <geoffroy.arenou@milton-innovation.com>
2023-06-16 15:52:20 +0200

    python

--- Python_3D/serialToOpen3D.py
+++ Python_3D/serialToOpen3D.py
@@ -1,6 +1,5 @@
 import open3d as o3d
 import numpy as np
-import time
 import serial
 
 ser = serial.Serial('COM6', 230400, timeout=0)
@@ -12,46 +11,49 @@ vis.create_window(height=480, width=640)
 
 # initialize pointcloud instance.
 pcd = o3d.geometry.PointCloud()
+
 # *optionally* add initial points
-points = np.random.rand(1, 3)
+#draw axes and center
+points = np.array([np.array([0, 0, 0]).tolist(), np.array([1, 0, 0]).tolist(),np.array([0, 1, 0]).tolist(),np.array([0, 0, 1]).tolist()])
+
 pcd.points = o3d.utility.Vector3dVector(points)
 
 # include it in the visualizer before non-blocking visualization.
 vis.add_geometry(pcd)
 
-# to add new points each dt secs.
-dt = 0.01
-# number of points that will be added
-n_new = 10
+opt = vis.get_render_option()
+opt.show_coordinate_frame = True
+#opt.background_color = np.asarray([0.5, 0.5, 0.5])
 
-previous_t = time.time()
 
-# run non-blocking visualization. 
+# run non-blocking visualization.
 # To exit, press 'q' or click the 'x' of the window.
 keep_running = True
 while keep_running:
-    
-    #if time.time() - previous_t > dt:
+
         try:
-            l = ser.readline()
-            line=l.decode("utf-8")
-            line_splitted = line.replace("\n","").replace("\r","").split(' ')
-            # Options (uncomment each to try them out):
-            # 1) extend with ndarrays.
-            t2=np.array([int(line_splitted[0],10)/1000, int(line_splitted[1],10)/1000, int(line_splitted[2],10)/1000]) 
-            t=np.fromstring(line, dtype=int, sep=' ')
-            t3=np.array([t2.tolist()])
-            pcd.points.extend(t3)
-            
+            if ser.in_waiting:
+                l = ser.readline()
+                line=l.decode("utf-8")
+                line_splitted = line.replace("\n","").replace("\r","").split(' ')
+                # Options (uncomment each to try them out):
+                # 1) extend with ndarrays.
+                x = int(line_splitted[0],10) / 1000 + 0.5
+                y = int(line_splitted[1],10) / 1000
+                z = int(line_splitted[2],10) / 1000 + 0.5
+                t1=np.array([x, y, z])
+                #t=np.fromstring(line, dtype=int, sep=' ')
+                t2=np.array([t1.tolist()])
+                pcd.points.extend(t2)
+
             # 2) extend with Vector3dVector instances.
             # pcd.points.extend(
             #o3d.utility.Vector3dVector(np.random.rand(n_new, 3))
-            
+
             # 3) other iterables, e.g
             #pcd.points.extend(np.random.rand(n_new, 3).tolist())
-            
+
             vis.update_geometry(pcd)
-            previous_t = time.time()
         except:
             continue
         keep_running = vis.poll_events()
--- Python_3D/testOpen3D.py
+++ Python_3D/testOpen3D.py
@@ -6,10 +6,13 @@ import time
 vis = o3d.visualization.Visualizer()
 vis.create_window(height=480, width=640)
 
+# number of points that will be added
+n_new = 500
+
 # initialize pointcloud instance.
 pcd = o3d.geometry.PointCloud()
 # *optionally* add initial points
-points = np.random.rand(10, 3)
+points = np.random.rand(n_new, 3)
 pcd.points = o3d.utility.Vector3dVector(points)
 
 # include it in the visualizer before non-blocking visualization.
@@ -17,28 +20,27 @@ vis.add_geometry(pcd)
 
 # to add new points each dt secs.
 dt = 0.01
-# number of points that will be added
-n_new = 10
 
 previous_t = time.time()
 
-# run non-blocking visualization. 
+# run non-blocking visualization.
 # To exit, press 'q' or click the 'x' of the window.
 keep_running = True
 while keep_running:
-    
+
     if time.time() - previous_t > dt:
         # Options (uncomment each to try them out):
         # 1) extend with ndarrays.
-        pcd.points.extend(np.random.rand(n_new, 3))
-        
+        t = np.random.rand(n_new, 3)
+        pcd.points.extend(t)
+
         # 2) extend with Vector3dVector instances.
         # pcd.points.extend(
         #o3d.utility.Vector3dVector(np.random.rand(n_new, 3))
-        
+
         # 3) other iterables, e.g
         #pcd.points.extend(np.random.rand(n_new, 3).tolist())
-        
+
         vis.update_geometry(pcd)
         previous_t = time.time()
 
